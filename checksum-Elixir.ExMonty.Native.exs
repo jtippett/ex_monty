c88db6cc2a8e93e048dac7543bbdd6959ed1bfe3
James Tippett <james.tippett@gmail.com>
2026-06-19 15:46:50 +0700

    Add precompiled NIF checksums for v0.4.0

--- checksum-Elixir.ExMonty.Native.exs
+++ checksum-Elixir.ExMonty.Native.exs
@@ -1,1 +1,6 @@
-%{}
+%{
+  "libex_monty-v0.4.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2ba968fd11e505b87de547ffc889e30067f945b6d2cd9465e2a148a9205ade30",
+  "libex_monty-v0.4.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:afdf6e3b4e84238ff5435dd717c144f63ecae547f5af298fc40c1ca33f822503",
+  "libex_monty-v0.4.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:7d2eab02daf19698bcee73ccdb791bc6615d3d671060baa2fe28a65f596dd41a",
+  "libex_monty-v0.4.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:10b3530c95c2b1d3f4d47a868e560783b00e562d42657ee757b25396410c1609",
+}
